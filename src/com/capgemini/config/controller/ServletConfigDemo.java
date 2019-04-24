3858ea7767a16522aac4c8635565c9eb3765c51c
Shakya <deepti.shakya@capgemini.com>
2019-04-24 19:32:32 +0530

    added

---
+++ src/com/capgemini/config/controller/ServletConfigDemo.java
@@ -0,0 +1,32 @@
+package com.capgemini.config.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = { "*.do" }, loadOnStartup = 1)
+public class ServletConfigDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		  response.setContentType("text/html");  
+		    PrintWriter out = response.getWriter();  
+		      
+		    ServletConfig config=getServletConfig();  
+		    String variable = config.getInitParameter("username");  
+		    out.print("username is: "+ variable);  
+		          
+		    out.close();  
+	}
+
+}
